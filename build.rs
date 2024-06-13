01fd251359ff72626784bcdf8abf90039e40ef72
Kentaro Tatsumi <paqira.2019@gmail.com>
2024-06-14 02:04:22 +0900

    fix build.rs (disable `f32/f64.c` when `f32/64-softfloat` enabled)

--- build.rs
+++ build.rs
@@ -1,13 +1,16 @@
 fn main() {
     let mut builder = cc::Build::new();
 
-    builder
-        .file("src/c/rounding_mode.c")
-        .file("src/c/f32.c")
-        .file("src/c/f64.c");
+    builder.file("src/c/rounding_mode.c");
 
     #[cfg(feature = "f16")]
     builder.file("src/c/f16.c");
+
+    #[cfg(not(feature = "f32-softfloat"))]
+    builder.file("src/c/f32.c");
+
+    #[cfg(not(feature = "f64-softfloat"))]
+    builder.file("src/c/f64.c");
 
     #[cfg(feature = "f128")]
     builder.file("src/c/f128.c");
