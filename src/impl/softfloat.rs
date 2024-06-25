8afea8c91d43d742faa55670b533e5a42e4794f1
Kentaro Tatsumi <paqira.2019@gmail.com>
2024-06-26 01:41:37 +0900

    stop import rounding_mode of softfloat.rs when any softfloat off

--- src/impl/softfloat.rs
+++ src/impl/softfloat.rs
@@ -6,4 +6,10 @@ pub(crate) mod f16;
 pub(crate) mod f32;
 #[cfg(feature = "f64-softfloat")]
 pub(crate) mod f64;
+#[cfg(any(
+    feature = "f128-softfloat",
+    feature = "f16-softfloat",
+    feature = "f32-softfloat",
+    feature = "f64-softfloat"
+))]
 pub(crate) mod rounding_mode;
